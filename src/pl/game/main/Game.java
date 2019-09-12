b33d51f6672cc062c95ab2b584b4d72d2610b75e
Igor <igorapli31@gmail.com>
2019-09-12 15:40:36 +0200

    Starting game project

---
+++ src/pl/game/main/Game.java
@@ -0,0 +1,22 @@
+package pl.game.main;
+
+import java.awt.Canvas;
+
+public class Game extends Canvas implements Runnable {
+	
+	private static final long serialVersionUID = 5749895903263842126L;
+	
+	public synchronized void start() {
+		
+	}
+	
+	public void run() {
+		
+	}
+	
+
+	public static void main(String args[]) {
+		
+	}
+
+}
