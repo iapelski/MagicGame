093dbd84f838ef016ac4451329c6c0f9bdb2c804
Igor <igorapli31@gmail.com>
2019-09-12 15:48:55 +0200

    First start of game api

--- src/pl/game/main/Game.java
+++ src/pl/game/main/Game.java
@@ -6,6 +6,11 @@ public class Game extends Canvas implements Runnable {
 	
 	private static final long serialVersionUID = 5749895903263842126L;
 	
+	public static final int WIDTH = 640, HEIGHT = WIDTH /12*9;
+	
+	public Game() {
+		new Window(WIDTH, HEIGHT, "Let's Build a game", this);
+	}
 	public synchronized void start() {
 		
 	}
@@ -16,7 +21,7 @@ public class Game extends Canvas implements Runnable {
 	
 
 	public static void main(String args[]) {
-		
+		new Game();
 	}
 
 }
