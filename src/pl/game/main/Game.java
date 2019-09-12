8eb3b5ff02641b2339fc730fe83f51b68797e6a5
Igor <igorapli31@gmail.com>
2019-09-12 15:54:01 +0200

    Adding Thread, and Exception

--- src/pl/game/main/Game.java
+++ src/pl/game/main/Game.java
@@ -7,12 +7,25 @@ public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = 5749895903263842126L;
 	
 	public static final int WIDTH = 640, HEIGHT = WIDTH /12*9;
+	private Thread thread;
+	private boolean running = false;
 	
 	public Game() {
 		new Window(WIDTH, HEIGHT, "Let's Build a game", this);
 	}
+	
 	public synchronized void start() {
-		
+		thread = new Thread(this);
+		thread.start();
+		running = true;
+	}
+	public synchronized void stop() {
+		try {
+			thread.join();
+			running = false;
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void run() {
