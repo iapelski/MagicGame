12ba03cff4f5d56a4fe15fc93037f613bbf13cf6
Igor <igorapli31@gmail.com>
2019-09-16 17:20:36 +0200

    adding Getter and Setter methods to GameObjcet

--- src/pl/game/main/GameObject.java
+++ src/pl/game/main/GameObject.java
@@ -17,5 +17,22 @@ public abstract class GameObject {
 	public abstract void tick();
 	public abstract void render(Graphics g);
 	
+	public void setX(int x) {
+		this.x = x;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
 	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setId(ID id) {
+		this.id = id;
+	}
 }
