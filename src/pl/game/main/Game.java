3eeb0fb7dbd7051a20dda72d29ecfafc0437b866
Igor <igorapli31@gmail.com>
2019-09-17 16:12:27 +0200

    adding key inputs working with different objects

--- src/pl/game/main/Game.java
+++ src/pl/game/main/Game.java
@@ -7,65 +7,65 @@ import java.awt.image.BufferStrategy;
 import java.util.Random;
 
 public class Game extends Canvas implements Runnable {
-	
+
 	private static final long serialVersionUID = 5749895903263842126L;
-	
-	public static final int WIDTH = 640, HEIGHT = WIDTH /12*9;
+
+	public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9;
 	private Thread thread;
 	private boolean running = false;
-	
+
 	private Random r;
 	private Handler handler;
-	
+
 	public Game() {
 		handler = new Handler();
 		this.addKeyListener(new KeyInput(handler));
-		
+
 		new Window(WIDTH, HEIGHT, "Let's Build a game", this);
-		
-		r= new Random();
-		
-		
-		handler.addObject(new Player((WIDTH/2-32), (HEIGHT/2-32), ID.Player));
-		handler.addObject(new Player((WIDTH/2+64), (HEIGHT/2-32), ID.Player2));
-		
-		
+
+		r = new Random();
+
+		handler.addObject(new Player((WIDTH / 2 - 32), (HEIGHT / 2 - 32), ID.Player));
+		handler.addObject(new Player((WIDTH / 2 + 64), (HEIGHT / 2 - 32), ID.Player2));
+
 	}
-	
+
 	public synchronized void start() {
 		thread = new Thread(this);
 		thread.start();
 		running = true;
 	}
+
 	public synchronized void stop() {
 		try {
 			thread.join();
 			running = false;
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void run() {
+		this.requestFocus();
 		long lastTime = System.nanoTime();
 		double amountOfTicks = 60.0;
 		double ns = 1000000000 / amountOfTicks;
 		double delta = 0;
 		long timer = System.currentTimeMillis();
 		int frames = 0;
-		while(running) {
+		while (running) {
 			long now = System.nanoTime();
 			delta += (now - lastTime) / ns;
 			lastTime = now;
-			while(delta >= 1) {
+			while (delta >= 1) {
 				tick();
 				delta--;
 			}
-			if(running) 
-				render(); 
+			if (running)
+				render();
 			frames++;
-			
-			if(System.currentTimeMillis()- timer > 1000) {
+
+			if (System.currentTimeMillis() - timer > 1000) {
 				timer += 1000;
 				System.out.println("FPS: " + frames);
 				frames = 0;
@@ -73,23 +73,24 @@ public class Game extends Canvas implements Runnable {
 		}
 		stop();
 	}
-	
+
 	private void tick() {
 		handler.tick();
-		
+
 	}
+
 	private void render() {
 		BufferStrategy bs = this.getBufferStrategy();
-		if(bs == null) {
+		if (bs == null) {
 			this.createBufferStrategy(3);
 			return;
 		}
-		
+
 		Graphics g = bs.getDrawGraphics();
-		
+
 		g.setColor(Color.black);
 		g.fillRect(0, 0, WIDTH, HEIGHT);
-		
+
 		handler.render(g);
 		g.dispose();
 		bs.show();
--- src/pl/game/main/Player.java
+++ src/pl/game/main/Player.java
@@ -5,27 +5,25 @@ import java.awt.Graphics;
 import java.util.Random;
 
 public class Player extends GameObject {
-	
+
 	Random r = new Random();
 
 	public Player(int x, int y, ID id) {
 		super(x, y, id);
-		
 
-		
 	}
 
-	
 	public void tick() {
 		x += velX;
 		y += velY;
 	}
 
-	
 	public void render(Graphics g) {
-		if(id == ID.Player) g.setColor(Color.white);
-		else if(id == ID.Player2) g.setColor(Color.blue);
-		
+		if (id == ID.Player)
+			g.setColor(Color.white);
+		else if (id == ID.Player2)
+			g.setColor(Color.blue);
+
 		g.fillRect(x, y, 32, 32);
 	}
 
